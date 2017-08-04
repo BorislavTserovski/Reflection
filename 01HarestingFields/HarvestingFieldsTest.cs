24d5d44546046d2e5b601212281b5224b26b297e
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-08-04 19:50:34 +0300

    2Tasks

---
+++ 01HarestingFields/HarvestingFieldsTest.cs
@@ -0,0 +1,86 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace _01HarestingFields
+{
+    using System;
+
+    class HarvestingFieldsTest
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            Type type = typeof(HarvestingFields);
+            while (input!="HARVEST")
+            {
+                switch (input)
+                {
+                    case "private":
+                        FieldInfo[] privateFields = type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic);
+                        foreach (FieldInfo pf in privateFields)
+                        {
+                            if (pf.IsPrivate)
+                            {
+                                sb.AppendLine($"private {pf.FieldType.Name} {pf.Name}");
+                            }
+                            
+                        }
+                        Console.WriteLine(sb.ToString().Trim());
+                        sb.Clear();
+                        break;
+                    case "public":
+                        FieldInfo[] publicFields = type.GetFields(BindingFlags.Instance | BindingFlags.Public);
+                        foreach (FieldInfo pubf in publicFields)
+                        {
+                            if (pubf.IsPublic)
+                            {
+                                sb.AppendLine($"public {pubf.FieldType.Name} {pubf.Name}");
+                            }
+                        }
+                        Console.WriteLine(sb.ToString().Trim());
+                        sb.Clear();
+                        break;
+                    case "protected":
+                        FieldInfo[] protectedFields = type.GetFields(BindingFlags.Instance | BindingFlags.Static|BindingFlags.NonPublic
+                            |BindingFlags.Public);
+                        foreach (FieldInfo prf in protectedFields)
+                        {
+                            if (prf.IsFamily)
+                            {
+                                sb.AppendLine($"protected {prf.FieldType.Name} {prf.Name}");
+                            }
+                        }
+                        Console.WriteLine(sb.ToString().Trim());
+                        sb.Clear();
+                        break;
+                    case "all":
+                        FieldInfo[] allFields = type.GetFields(BindingFlags.Instance | BindingFlags.Static|BindingFlags.NonPublic
+                            |BindingFlags.Public);
+                        string access = "";
+                        foreach (FieldInfo af in allFields)
+                        {
+                            if (af.IsFamily)
+                            {
+                                access = "protected";
+                            }
+                            else if (af.IsPrivate)
+                            {
+                                access = "private";
+                            }
+                            else if (af.IsPublic)
+                            {
+                                access = "public";
+                            }
+                             sb.AppendLine($"{access} {af.FieldType.Name} {af.Name}");
+                        }
+                        Console.WriteLine(sb.ToString().Trim());
+                        sb.Clear();
+                        break;
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ 02BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Reflection;
+using System.Text;
+
+
+using System;
+
+class BlackBoxIntegerTests
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        Type type = typeof(BlackBoxInt);
+        var methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+        var instance = Activator.CreateInstance(type, true);
+        var fields = type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+        StringBuilder result = new StringBuilder();
+        while (input != "END")
+        {
+            string[] args = input.Split('_');
+            string command = args[0];
+            int number = int.Parse(args[1]);
+            methods.FirstOrDefault(m => m.Name == command)?.Invoke(instance, new object[] { number });
+
+            foreach (var field in fields)
+            {
+                result.AppendLine(field.GetValue(instance).ToString());
+            }
+
+            input = Console.ReadLine();
+        }
+        Console.WriteLine(result.ToString().Trim());
+    }
+}
+
